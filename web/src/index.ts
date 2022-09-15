b4b4992c9371ecb11cb31f2c03f548eba772c696
Joe Polny <joepolny@gmail.com>
2022-09-14 20:42:24 -0400

    add buttons

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,3 +1,4 @@
+import MyAlgoConnect from '@randlabs/myalgo-connect'
 import algosdk from 'algosdk'
 import { Auction } from './beaker/auction_client'
 import { MyAlgoSession } from './wallets/myalgo'
@@ -5,13 +6,20 @@ import { MyAlgoSession } from './wallets/myalgo'
 const myAlgo = new MyAlgoSession()
 const algodClient = new algosdk.Algodv2('', 'https://testnet-api.algonode.cloud', '')
 
+const connectBtn = document.getElementById('connect') as HTMLButtonElement
+const createBtn = document.getElementById('create') as HTMLButtonElement
+
 async function signer (txns: algosdk.Transaction[]) {
   const sTxns = await myAlgo.signTxns(txns)
   return sTxns.map(s => s.blob)
 }
 
-(async function () {
+connectBtn.onclick = async () => {
   await myAlgo.getAccounts()
+  createBtn.disabled = false
+}
+
+createBtn.onclick = async () => {
   const account = myAlgo.accounts[0]
 
   const appClient = new Auction({
@@ -22,4 +30,4 @@ async function signer (txns: algosdk.Transaction[]) {
 
   const [appId, appAddr, txId] = await appClient.create()
   document.getElementById('status').innerHTML = `App created with id ${appId} and address ${appAddr} in tx ${txId}`
-})()
+}
