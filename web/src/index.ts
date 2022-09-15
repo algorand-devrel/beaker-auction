5f2fcd91cb34fea999f098278e751e6ab84d065e
Joe Polny <joepolny@gmail.com>
2022-09-14 21:48:46 -0400

    working bids

--- web/src/index.ts
+++ web/src/index.ts
@@ -6,28 +6,85 @@ import { MyAlgoSession } from './wallets/myalgo'
 const myAlgo = new MyAlgoSession()
 const algodClient = new algosdk.Algodv2('', 'https://testnet-api.algonode.cloud', '')
 
-const connectBtn = document.getElementById('connect') as HTMLButtonElement
-const createBtn = document.getElementById('create') as HTMLButtonElement
+const buttonIds = ['create', 'connect', 'start', 'bid']
+const buttons: { [key: string]: HTMLButtonElement } = {}
+const accountsMenu = document.getElementById('accounts') as HTMLSelectElement
+const amountInput = document.getElementById('amount') as HTMLInputElement
+
+let appId: number
+
+buttonIds.forEach(id => {
+  buttons[id] = document.getElementById(id) as HTMLButtonElement
+})
 
 async function signer (txns: algosdk.Transaction[]) {
   const sTxns = await myAlgo.signTxns(txns)
   return sTxns.map(s => s.blob)
 }
 
-connectBtn.onclick = async () => {
+buttons.connect.onclick = async () => {
   await myAlgo.getAccounts()
-  createBtn.disabled = false
+  buttons.create.disabled = false
+  myAlgo.accounts.forEach(account => {
+    accountsMenu.add(new Option(`${account.name} - ${account.address}`, account.address))
+  })
 }
 
-createBtn.onclick = async () => {
-  const account = myAlgo.accounts[0]
-
-  const appClient = new Auction({
+buttons.create.onclick = async () => {
+  const auctionApp = new Auction({
     client: algodClient,
     signer,
-    sender: account.address
+    sender: accountsMenu.selectedOptions[0].value
   })
 
-  const [appId, appAddr, txId] = await appClient.create()
-  document.getElementById('status').innerHTML = `App created with id ${appId} and address ${appAddr} in tx ${txId}`
+  const [createdAppId, appAddr, txId] = await auctionApp.create()
+  appId = createdAppId
+  document.getElementById('status').innerHTML = `App created with id ${appId} and address ${appAddr} in tx ${txId}. See it <a href='https://testnet.algoexplorer.io/application/${appId}'>here</a>`
+  buttons.start.disabled = false
+  buttons.create.disabled = true
 }
+
+buttons.start.onclick = async () => {
+  const auctionApp = new Auction({
+    client: algodClient,
+    signer,
+    sender: accountsMenu.selectedOptions[0].value,
+    appId
+  })
+
+  const payment = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+    suggestedParams: await algodClient.getTransactionParams().do(),
+    amount: 100_000,
+    from: accountsMenu.selectedOptions[0].value,
+    to: algosdk.getApplicationAddress(appId)
+  })
+
+  await auctionApp.start_auction({
+    payment,
+    starting_price: BigInt(amountInput.valueAsNumber),
+    length: BigInt(36_000)
+  })
+
+  buttons.bid.disabled = false
+  buttons.start.disabled = true
+}
+
+buttons.bid.onclick = async () => {
+    const auctionApp = new Auction({
+      client: algodClient,
+      signer,
+      sender: accountsMenu.selectedOptions[0].value,
+      appId
+    })
+  
+    const payment = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+      suggestedParams: await algodClient.getTransactionParams().do(),
+      amount: amountInput.valueAsNumber,
+      from: accountsMenu.selectedOptions[0].value,
+      to: algosdk.getApplicationAddress(appId)
+    })
+  
+    await auctionApp.bid({
+      payment,
+    })
+  }
