5a6d200d09e3dbd2a93da8b2404c9c0b294ead5b
Joe Polny <joepolny@gmail.com>
2022-10-21 08:33:44 -0400

    use future.transaction

--- test_auction.py
+++ test_auction.py
@@ -1,11 +1,12 @@
 from beaker import *
 from auction import *
-from algosdk import transaction
+from algosdk.future import transaction
 from algosdk.encoding import encode_address
 from algosdk.atomic_transaction_composer import TransactionWithSigner
 import pytest
 
 MIN_FEE = 1_000
+
 
 @pytest.fixture(scope="module")
 def create_app():
@@ -29,6 +30,7 @@ def create_app():
 
     app_client.create()
 
+
 @pytest.fixture(scope="module")
 def start_auction():
     sp = app_client.get_suggested_params()
@@ -38,10 +40,7 @@ def start_auction():
             sender=creator_acct.address,
             receiver=app_client.app_addr,
             amt=100_000,
-            fee=sp.fee,
-            first=sp.first,
-            last=sp.last,
-            gh=sp.gh,
+            sp=sp,
         ),
         signer=creator_acct.signer,
     )
@@ -49,6 +48,7 @@ def start_auction():
     app_client.call(
         Auction.start_auction, payment=pay_txn, starting_price=10_000, length=36_000
     )
+
 
 @pytest.fixture(scope="module")
 def send_first_bid():
@@ -59,18 +59,18 @@ def send_first_bid():
 
     pay_txn = TransactionWithSigner(
         txn=transaction.PaymentTxn(
-            sender=first_bidder.address,
-            receiver=app_client.app_addr,
-            amt=20_000,
-            fee=sp.fee,
-            first=sp.first,
-            last=sp.last,
-            gh=sp.gh,
+            sender=first_bidder.address, receiver=app_client.app_addr, amt=20_000, sp=sp
         ),
         signer=first_bidder.signer,
     )
 
-    app_client.call(Auction.bid, payment=pay_txn, previous_bidder=first_bidder.address, signer=first_bidder.signer)
+    app_client.call(
+        Auction.bid,
+        payment=pay_txn,
+        previous_bidder=first_bidder.address,
+        signer=first_bidder.signer,
+    )
+
 
 @pytest.fixture(scope="module")
 def send_second_bid():
@@ -79,6 +79,7 @@ def send_second_bid():
     second_bidder = accounts.pop()
 
     sp = app_client.get_suggested_params()
+    sp.fee = MIN_FEE * 2
     first_bidder_amount = app_client.client.account_info(first_bidder.address)["amount"]
 
     pay_txn = TransactionWithSigner(
@@ -86,10 +87,7 @@ def send_second_bid():
             sender=second_bidder.address,
             receiver=app_client.app_addr,
             amt=30_000,
-            fee=MIN_FEE*2,
-            first=sp.first,
-            last=sp.last,
-            gh=sp.gh,
+            sp=sp,
         ),
         signer=second_bidder.signer,
     )
@@ -101,55 +99,68 @@ def send_second_bid():
         signer=second_bidder.signer,
     )
 
+
 ##############
 # create tests
 ##############
+
 
 @pytest.mark.create
 def test_create_owner(create_app):
     addr = bytes.fromhex(app_client.get_application_state()["owner"])
     assert encode_address(addr) == creator_acct.address
 
+
 @pytest.mark.create
 def test_create_highest_bidder(create_app):
     assert app_client.get_application_state()["highest_bidder"] == ""
+
 
 @pytest.mark.create
 def test_create_highest_bid(create_app):
     assert app_client.get_application_state()["highest_bid"] == 0
 
+
 @pytest.mark.create
 def test_create_auction_end(create_app):
     assert app_client.get_application_state()["auction_end"] == 0
+
 
 #####################
 # start_auction tests
 #####################
 
+
 @pytest.mark.start_auction
 def test_start_auction_end(create_app, start_auction):
     assert app_client.get_application_state()["auction_end"] != 0
+
 
 @pytest.mark.start_auction
 def test_start_auction_highest_bid(create_app, start_auction):
     assert app_client.get_application_state()["highest_bid"] == 10_000
 
+
 #################
 # first_bid tests
 #################
 
+
 @pytest.mark.first_bid
 def test_first_bid_highest_bid(create_app, start_auction, send_first_bid):
     assert app_client.get_application_state()["highest_bid"] == 20_000
+
 
 @pytest.mark.first_bid
 def test_first_bid_highest_bidder(create_app, start_auction, send_first_bid):
     addr = bytes.fromhex(app_client.get_application_state()["highest_bidder"])
     assert encode_address(addr) == first_bidder.address
 
+
 ##################
 # second_bid tests
 ##################
+
 
 @pytest.mark.second_bid
 def test_second_bid_highest_bid(
@@ -165,6 +176,7 @@ def test_second_bid_highest_bidder(
     addr = bytes.fromhex(app_client.get_application_state()["highest_bidder"])
     assert encode_address(addr) == second_bidder.address
 
+
 @pytest.mark.second_bid
 def test_second_bid_first_bidder_balance(
     create_app, start_auction, send_first_bid, send_second_bid
@@ -173,6 +185,7 @@ def test_second_bid_first_bidder_balance(
         app_client.client.account_info(first_bidder.address)["amount"]
         == first_bidder_amount + 20_000
     )
+
 
 @pytest.mark.second_bid
 def test_second_bid_app_balance(
