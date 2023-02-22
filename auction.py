920fa4abd61268c9109717c55fd90c0c48536907
Joe Polny <joepolny@gmail.com>
2023-02-21 22:14:39 -0500

    use app.dump()

--- auction.py
+++ auction.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 from pyteal import *
 from beaker import *
-import os
-import json
 from typing import Final
 
 
@@ -156,26 +154,4 @@ class Auction(Application):
 if __name__ == "__main__":
     app = Auction(version=8)
 
-    if os.path.exists("approval.teal"):
-        os.remove("approval.teal")
-
-    if os.path.exists("approval.teal"):
-        os.remove("clear.teal")
-
-    if os.path.exists("abi.json"):
-        os.remove("abi.json")
-
-    if os.path.exists("app_spec.json"):
-        os.remove("app_spec.json")
-
-    with open("approval.teal", "w") as f:
-        f.write(app.approval_program)
-
-    with open("clear.teal", "w") as f:
-        f.write(app.clear_program)
-
-    with open("abi.json", "w") as f:
-        f.write(json.dumps(app.contract.dictify(), indent=4))
-
-    with open("app_spec.json", "w") as f:
-        f.write(json.dumps(app.application_spec(), indent=4))
+    app.dump()
